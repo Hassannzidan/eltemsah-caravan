b52ab64913902ea7bf9d14c4895e83441cde69ca
Hassannzidan <hzidan014@gmail.com>
2025-06-16 23:13:51 +0300

    delete the product card and product list item components and update the product list and product grid components accordingly

--- src/app/features/components/products-components/product-list/product-list.ts
+++ src/app/features/components/products-components/product-list/product-list.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../../../data/products.data';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css'
 })
 export class ProductList {
-
+  @Input() products: Product[] = [];
 }
