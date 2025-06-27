8e1a1b34810979e017ec0acb8ac292e1511956a2
Hassannzidan <hzidan014@gmail.com>
2025-06-28 02:29:14 +0300

    translate the mission-vision

--- src/app/features/components/about-components/mission-vision/mission-vision.ts
+++ src/app/features/components/about-components/mission-vision/mission-vision.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgIconsModule, provideIcons } from '@ng-icons/core';
+import { lucideStar } from '@ng-icons/lucide';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-mission-vision',
-  imports: [CommonModule],
+  imports: [CommonModule,NgIconsModule,TranslateModule],
   templateUrl: './mission-vision.html',
-  styleUrl: './mission-vision.css'
+  styleUrl: './mission-vision.css',
+  viewProviders:[provideIcons({
+    lucideStar
+  })]
 })
 export class MissionVision {
 
--- src/app/features/pages/about/about.ts
+++ src/app/features/pages/about/about.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { HeroSection } from "../../components/about-components/hero-section/hero-section";
 import { CompanyOverview } from "../../components/about-components/company-overview/company-overview";
 import { RequestQoute } from "../../components/home-components/request-qoute/request-qoute";
-import { CoreValues } from "../../components/about-components/core-values/core-values";
 import { MissionVision } from "../../components/about-components/mission-vision/mission-vision";
 
 @Component({
