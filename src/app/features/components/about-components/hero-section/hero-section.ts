194989541f4ef017cc556102dd77714a48edf41a
Hassannzidan <hzidan014@gmail.com>
2025-06-28 02:03:20 +0300

    .

--- src/app/features/components/about-components/hero-section/hero-section.ts
+++ src/app/features/components/about-components/hero-section/hero-section.ts
@@ -4,16 +4,21 @@ import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [CommonModule,TranslateModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.css',
 })
 export class HeroSection implements OnInit {
+  imageUrl = '';
   scrollY = 0;
   isMobile = false;
 
   ngOnInit() {
     this.isMobile = window.innerWidth < 768;
+    const isMobile = window.innerWidth < 768;
+    this.imageUrl = isMobile
+      ? 'https://images.unsplash.com/photo-1582489853490-cd3a53eb4530?q=50&w=800&auto=format' // نسخة خفيفة
+      : 'https://images.unsplash.com/photo-1582489853490-cd3a53eb4530?q=80&w=2070&auto=format';
   }
 
   @HostListener('window:scroll', [])
