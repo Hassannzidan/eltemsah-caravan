f28dd166047b121ff2178f819233c9a2e3004f9c
Hassannzidan <hzidan014@gmail.com>
2025-06-28 19:59:58 +0300

    retouch the contact cards

--- src/app/features/components/contact-components/contact-cards/contact-cards.ts
+++ src/app/features/components/contact-components/contact-cards/contact-cards.ts
@@ -1,52 +1,62 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgIconsModule, provideIcons } from '@ng-icons/core';
-import { lucideMail, lucidePhone } from '@ng-icons/lucide';
+import { lucideClock, lucideMail, lucidePhone } from '@ng-icons/lucide';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-cards',
-  imports: [NgIconsModule,TranslateModule,CommonModule],
+  imports: [NgIconsModule, TranslateModule, CommonModule],
   templateUrl: './contact-cards.html',
   styleUrl: './contact-cards.css',
-  viewProviders:[provideIcons({
-    lucidePhone,
-    lucideMail
-  })]
+  viewProviders: [
+    provideIcons({
+      lucidePhone,
+      lucideMail,
+      lucideClock
+    }),
+  ],
 })
-export class ContactCards  {
-  email:string = 'Temsah.caravan@gmail.com';
+export class ContactCards {
+  email: string = 'Temsah.caravan@gmail.com';
 
-   contactCards = [
+  contactCards = [
     {
       id: 'phone',
       icon: 'lucidePhone',
-      titleKey: 'phone.title',
+      titleKey: 'contactCards.contact.phone.title',
       data: '+20 110 201 2009',
-      descriptionKey: 'phone.desc',
+      descriptionKey: 'contactCards.contact.phone.desc',
       bgColor: 'bg-sky-500 group-hover:scale-110',
       iconBgHover: 'bg-sky-500 group-hover:bg-sky-400',
-      ariaLabel: 'contact.phone.aria',
+      ariaLabel: 'contactCards.contact.phone.aria',
+      gradient: 'bg-gradient-to-r from-red-700/80 to-red-800',
+      bgGradient: 'bg-gradient-to-br from-red-50 to-red-100',
     },
     {
       id: 'email',
       icon: 'lucideMail',
-      titleKey: 'email.title',
+      titleKey: 'contactCards.contact.email.title',
       data: this.email,
-      descriptionKey: 'email.desc',
+      descriptionKey: 'contactCards.contact.email.desc',
       bgColor: 'bg-red-700/80 group-hover:scale-110',
       iconBgHover: 'bg-red-700 group-hover:bg-orange-500',
-      ariaLabel: 'contact.email.aria',
+      ariaLabel: 'contact.contactCards.contact.email.aria',
+      gradient: 'bg-gradient-to-r from-red-700/80 to-red-800',
+      bgGradient: 'bg-gradient-to-br from-red-50 to-red-100',
     },
     {
       id: 'hours',
-      svg: 'M12 2.25a9.75 9.75 0 110 19.5 9.75 9.75 0 010-19.5zM12 6v6.75l4.5 2.25',
-      titleKey: 'hours.title',
-      dataKey: 'hours.times',
-      descriptionKey: null,
+      icon: 'lucideClock',
+      titleKey: 'contactCards.contact.hours.title',
+      dataKey: 'contactCards.contact.hours.times',
+      descriptionKey: 'contactCards.contact.hours.times',
       bgColor: 'bg-green-700 group-hover:scale-110',
       iconBgHover: 'bg-green-700 group-hover:bg-green-500',
-      ariaLabel: 'contact.hours.aria',
+      ariaLabel: 'contactCards.contact.hours.aria',
+      gradient: 'bg-gradient-to-r from-red-700/80 to-red-800',
+      bgGradient: 'bg-gradient-to-br from-red-50 to-red-100',
+      data:'contactCards.contact.hours.data'
     },
   ];
 }
--- src/app/features/components/contact-components/hero-section/hero-section.ts
+++ src/app/features/components/contact-components/hero-section/hero-section.ts
@@ -1,123 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener, inject, OnInit } from '@angular/core';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { LanguageService } from '../../../../services/language/language.service';
-import gsap from 'gsap';
+import { NgIconsModule, provideIcons } from '@ng-icons/core';
+import { lucideAward, lucideSalad, lucideShield, lucideStar } from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule , NgIconsModule],
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.css',
+  viewProviders:[provideIcons({
+    lucideStar,
+    lucideShield,
+    lucideAward
+  })]
 })
 export class HeroSection implements OnInit {
   scrollY = 0;
   currentLang: 'en' | 'ar' = 'en';
-  private translate = inject(TranslateService);
   private langService = inject(LanguageService);
-
-  // private textArray = [
-  //   "We love questions and feedback – don’t hesitate to reach out. We're here to help!",
-  // ];
-
-  // private typingElementIds = ['#first-text'];
-  // private currentTextIndex = 0;
-  // private currentCharIndex = 0;
-  // private typingSpeed = 100;
-  // private typingInterval: any;
 
   ngOnInit(): void {
     this.currentLang = this.langService.getCurrentLanguage();
-
-    this.translate.onLangChange.subscribe((event) => {
-      this.currentLang = event.lang as 'en' | 'ar';
-      this.animateTyping();
-    });
   }
-
-  ngAfterViewInit(): void {
-    this.animateTyping();
-  }
-
-  private animateTyping(): void {
-    this.translate.get('contactCards.typing_text').subscribe((text: string) => {
-      const target = document.getElementById('typing-text');
-      if (!target) return;
-
-      gsap.killTweensOf(target);
-      target.innerText = '';
-
-      const chars = text.split('');
-      const typingSpeed = this.currentLang === 'ar' ? 0.1 : 0.06;
-
-      const tl = gsap.timeline();
-
-      chars.forEach((_, index) => {
-        tl.to(
-          {},
-          {
-            duration: typingSpeed,
-            onUpdate: () => {
-              target!.innerText = text.substring(0, index + 1);
-            },
-          }
-        );
-      });
-
-      // تكرار بعد 2 ثانية
-      tl.to(
-        {},
-        {
-          duration: 2,
-          onComplete: () => {
-            target!.innerText = '';
-            this.animateTyping();
-          },
-        }
-      );
-    });
-  }
-
-  // private loadTypingText(): void {
-  //   this.translate
-  //     .get('contactCards.typing_text')
-  //     .subscribe((translatedText: string) => {
-  //       this.textArray = [translatedText];
-  //       this.startTypingEffect();
-  //     });
-  // }
-
-  // private startTypingEffect(): void {
-  //   this.typeNextText();
-  // }
-
-  // private typeNextText(): void {
-  //   const text = this.textArray[this.currentTextIndex];
-  //   const currentId = this.typingElementIds[0];
-  //   const currentElement = document.querySelector<HTMLElement>(currentId);
-
-  //   if (!currentElement) return;
-
-  //   currentElement.innerHTML = '';
-  //   currentElement.classList.add('typing');
-
-  //   let interval = setInterval(() => {
-  //     if (this.currentCharIndex < text.length) {
-  //       currentElement.innerHTML += text[this.currentCharIndex];
-  //       this.currentCharIndex++;
-  //     } else {
-  //       clearInterval(interval);
-  //       this.currentCharIndex = 0;
-  //       currentElement.classList.remove('typing');
-
-  //       // بدل ما تمسح عنصرين، امسح واحد فقط
-  //       setTimeout(() => {
-  //         currentElement.innerHTML = '';
-  //         this.typeNextText();
-  //       }, 1500);
-  //     }
-  //   }, this.typingSpeed);
-  // }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
